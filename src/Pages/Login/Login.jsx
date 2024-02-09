901d6c37e11ef57e04afea1a2b5dd0b93fc53ade
RamRavipati0406 <ravipati@uwm.edu>
2024-02-08 21:24:17 -0600

    login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,39 @@
-import React, { useState } from 'react' 
+import React, { useState , useEffect} from 'react' 
 import './Login.css'
 import email_icon from '../../Assets/email.png'
 import password_lock from '../../Assets/password.png'
 export const Login = () => {
 
-  const [action,setAction] = useState("Login");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    fetch('your-endpoint-url', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+    })
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    })
+    .then(data => {
+      // Handle your data here
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }, []);
+
   return (
     <div className='container'> 
      <div className="header">
@@ -15,11 +44,11 @@ export const Login = () => {
      <div className="inputs">
      <div className="input">
       <img src={email_icon} alt="" />
-      <input type="email" placeholder='Email'/>
+      <input type="email" placeholder='Email' onChange={e => setEmail(e.target.value)}/>
      </div>
      <div className="input">
       <img src={password_lock} alt="" />
-      <input type="password" placeholder='Password' />
+      <input type="password" placeholder='Password' onChange={e =>setPassword(e.target.value)}/>
      </div>
      </div>
      <div className="forgot-password">Lost Password?<span>Click Here!</span></div>
